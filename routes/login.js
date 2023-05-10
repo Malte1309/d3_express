eafb05629b4feda94cadf2ac99ef300b88511f96
radosk <malte-leon.schulz@gmx.de>
2023-05-10 20:38:12 +0200

    Routen erstellt - fehlerhaft

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
-  res.render('login')
+router.get('/login', function(req, res, next) {
+  res.get('login.html');
+  //res.get('login')
+  //res.render('login')
 });
 
 module.exports = router;
